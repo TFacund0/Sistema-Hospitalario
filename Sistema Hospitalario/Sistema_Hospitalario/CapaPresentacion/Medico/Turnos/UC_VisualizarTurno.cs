656ce7349af7c1b8e81e40a0b2edcee22f82f80d
Tobias <tobiascesarfacundoacevedo@gmail.com>
2025-11-12 07:48:20 -0300

    nada

---
+++ Sistema Hospitalario/Sistema_Hospitalario/CapaPresentacion/Medico/Turnos/UC_VisualizarTurno.cs
@@ -0,0 +1,15 @@
+﻿using Sistema_Hospitalario.CapaNegocio.DTOs.TurnoDTO;
+using System.Windows.Forms;
+
+namespace Sistema_Hospitalario.CapaPresentacion.Medico.Turnos
+{
+    internal class UC_VisualizarTurno : UserControl
+    {
+        private TurnoDto turno;
+
+        public UC_VisualizarTurno(TurnoDto turno)
+        {
+            this.turno = turno;
+        }
+    }
+}
